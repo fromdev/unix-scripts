e61dedd342fc673e418a9e7327df99a2ec875272
Sachin Joshi <software.wikipedia@gmail.com>
2016-09-13 15:12:32 -0700

    Cleanup slow mac
    Clean up the unwanted caching directories and clutter in mac to make some room and speed up your mac.

---
+++ bin/slowmac.sh
@@ -0,0 +1,7 @@
+rm -rf /System/Library/Caches
+rm -rf /Library/Caches
+rm -rf /Library/LaunchAgents
+rm -rf /Library/LaunchDaemons
+rm -rf /Library/StartupItems
+rm -rf ~/Library/Caches
+rm -rf ~/Library/LaunchAgents
